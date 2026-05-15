913207e82cb408659817dd0d773d101ec900e4fb
Karan <karanbir97@outlook.com>
2026-05-14 20:51:04 -0500

    Initial Commit

---
+++ HMI/HistoryDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sqlite;
+
+namespace HMI
+{
+    class HistoryDbService
+    {
+    }
+}
---
+++ HMI/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TwinCAT.Ads;
+
+namespace HMI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string amsNetId = "192.168.1.10.1.1";
+        private int adsPort = 851;
+        private AdsClient ?adsClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectToPlc();
+        }
+
+        public void ConnectToPlc()
+        {
+            try
+            {
+                adsClient = new AdsClient();
+                adsClient.Connect(amsNetId, adsPort);
+                //adsClient.ReadValue("MAIN.bAutoMode",typeof(bool));
+                adsClient.ReadValue("MAIN.DoesNotExist", typeof(bool));
+
+                lblStatus.Content = "PLC: Connected";
+            }
+            catch
+            {
+                lblStatus.Content = "PLC: Offline";
+            }
+
+        }
+
+       
+    }
+}
